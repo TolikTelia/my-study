6f3a0828c0d83c9bd4df30fca73ba97d2d0c668d
Mike.Gusev <mike.gusev@hindsiteinc.com>
2020-03-11 12:21:50 +0200

    renamed folder

--- src/reactLearn/reactLearn.js
+++ src/reactLearn/reactLearn.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ContextLearn from "./contextLearn/contextLearn";
 import HooksLearn from './hooksLearn/hooksLearn.js'
-import ReduxLearn from './redux|routing|Learn/reduxLearn'
+import ReduxLearn from './reduxRoutingLearn/reduxLearn'
 import RoutingLearn from './routingLearn/routingLearn'
 
 const ReactLearn = () => {
