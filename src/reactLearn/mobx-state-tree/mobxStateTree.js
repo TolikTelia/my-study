d4b0e6123a8b039c7ab2fe5efe9316e4e9f4d4a7
Mike.Gusev <mike.gusev@hindsiteinc.com>
2020-03-23 13:43:42 +0200

    mobx state tree

--- src/reactLearn/mobx-state-tree/mobxStateTree.js
+++ src/reactLearn/mobx-state-tree/mobxStateTree.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import store from './store'
+import { observer } from 'mobx-react';
 
 const MobxStateTree = () => {
+    const {
+        counter,
+        prev,
+        next,
+        increment,
+        decrement,
+        snapshotsCount,
+    } = store;
+
     return (
         <div>
-
+            <p>sanpshots {snapshotsCount}</p>
+            <button onClick={prev}>prev</button>
+            <button onClick={next}>next</button>
+            <h6>{counter}</h6>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
         </div>
     );
 };
 
-export default MobxStateTree;
+export default observer(MobxStateTree);
